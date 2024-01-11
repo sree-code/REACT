abc30a3ec2ca006e323330aa728cec960aa06699
SREE <gorantla899@gmail.com>
2024-01-11 13:39:03 -0600

    first commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+
+  return (
+    <div>
+      <header>TS AMAZON</header>
+      <main></main>
+      <footer>ALL RIGHTS ARE RESERVED</footer>
+    </div>
+  );
+}
+
+export default App
